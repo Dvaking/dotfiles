c8cb908a1fed9126c51677748338cf37fa0c0db4
Dvaking <marc.mendia@gmail.com>
2025-02-21 14:24:46 +0100

    [UPD] fedora install for 41

--- fedora.sh
+++ fedora.sh
@@ -14,7 +14,7 @@ display() {
     local header_text="$1"
     local DISPLAY_COMMAND="echo"
 
-    if [ "$(command -v figlet)" ]; then
+    if command -v figlet &> /dev/null; then
         DISPLAY_COMMAND="figlet"
     fi
 
@@ -39,43 +39,40 @@ fi
 # Configuration
 START=$(date +%s)
 LOG_FILE="/var/log/installation.log"
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 CRONTAB_ROOT="$SCRIPT_DIR/crontab/root"
 mkdir -p "$HOME/.config/" "$HOME/Epitech"
 
-# define what you want to install
-INSTALL_DOCKER=true
-
+# Define what you want to install
+INSTALL_DOCKER=false
 INSTALL_SFML=true
-INSTALL_NCURSES=true;
-
+INSTALL_NCURSES=true
 INSTALL_JAVA=true
 INSTALL_C=true
 INSTALL_PYTHON=true
 INSTALL_JS=true
 INSTALL_TS=true
 
-# Update Submodule
+# Update Submodule
 git submodule update --init --recursive
 
-
-# Update DNF
+# Update DNF configuration and system
 sudo cp "$SCRIPT_DIR/dnf/dnf.conf" /etc/dnf/dnf.conf
 sudo dnf update -y
 
-# default APP
-sudo dnf install -y htop vim curl figlet neofetch
-sudo dnf group install -y 'Development Tools'
+# Install default apps
+sudo dnf install -y htop vim curl figlet
+sudo dnf group install development-tools
 
 display "git"
 sudo dnf install -y git
 
 display "ZSH"
-if [ ! "$(command -v zsh)" ]; then
+if ! command -v zsh &>/dev/null; then
     sudo dnf install -y zsh fontawesome-fonts    
 fi
 cp "$SCRIPT_DIR/zsh/.zshrc" "$HOME/.zshrc"
-mkdir "$HOME/.zsh"
+mkdir -p "$HOME/.zsh"
 cp "$SCRIPT_DIR/zsh/alias.zsh" "$HOME/.zsh"
 cp "$SCRIPT_DIR/zsh/env.zsh" "$HOME/.zsh"
 touch "$HOME/.zsh/kubectl.zsh"
@@ -85,10 +82,9 @@ sudo dnf install -y flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 log "End Flatpak"
 
-#default language
-
+# Install Rust
 display "Start Rust"
-if [ ! "$(command -v cargo)" ]; then
+if ! command -v cargo &>/dev/null; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rust.sh
     chmod +x /tmp/rust.sh
     /tmp/rust.sh -y
@@ -135,94 +131,88 @@ fi
 
 if [ "$INSTALL_C" == true ]; then
     display "C Start"
-    sudo dnf group install -y 'C Development Tools and Libraries'
     "$SCRIPT_DIR/criterion/install_criterion.sh"
     log "C End"
 fi
 
 if [ "$INSTALL_DOCKER" == true ]; then
-    display "Start Docker Engine" # docker
-    if [ ! "$(command -v docker)" ]; then
+    display "Start Docker Engine"
+    if ! command -v docker &>/dev/null; then
         sudo dnf -y install dnf-plugins-core
         sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-        echo "Creating group: docker"
-        sudo groupadd docker
+        sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        if ! getent group docker &>/dev/null; then
+            echo "Creating group: docker"
+            sudo groupadd docker
+        fi
         sudo usermod -aG docker "$USER"
-        sudo newgrp docker
+        sudo systemctl enable containerd.service
+        sudo systemctl enable docker.service
     fi
     log "End Docker Engine"
 fi
 
-
 display "Start Terminal Emulators" # kitty
-
 sudo dnf install -y kitty
 mkdir -p "$HOME/.config/kitty/"
 cp "$SCRIPT_DIR/kitty/kitty.conf" "$HOME/.config/kitty/"
 
-display "Start Modern replacement"
-cargo install eza fcp
-sudo npm i -g safe-rm
-sudo dnf install -y tldr bat ripgrep fzf fd-find
-log "End Modern replacement"
+# display "Start Modern replacement"
+# cargo install eza fcp
+# sudo npm install -g safe-rm
+# sudo dnf install -y tldr bat ripgrep fzf fd-find
+#log "End Modern replacement"
 
 display "Start File Managers"
 
-# terminal base
-if [ ! "$(command -v yazi)" ]; then
+# Install yazi
+if ! command -v yazi &>/dev/null; then
     cargo install --locked yazi-fm
 fi
 
-#add font on terminal
-sudo mkdir ~/.local/share/font/
+# Add font to terminal
+sudo mkdir -p ~/.local/share/font/
 sudo cp -r terminal_font/ ~/.local/share/font/
 
-#folder creation
-
-mkdir "$HOME/Github_Public_Porfile/"
-mkdir "$HOME/Project_Epitech/"
-mkdir "$HOME/Personnal_Project/"
+# Create folders
+mkdir -p "$HOME/Github_Public_Profile/"
+mkdir -p "$HOME/Project_Epitech/"
+mkdir -p "$HOME/Personal_Project/"
 
 display "Start Communication"
 
-# discord
-if [ ! "$(command -v Discord)" ]; then
+# Discord
+if ! command -v Discord &>/dev/null; then
     sudo flatpak install -y flathub com.discordapp.Discord
 fi
 
-# teams for linux
-if [ ! "$(command -v teams-for-linux)" ]; then
+# Teams for Linux
+if ! command -v teams-for-linux &>/dev/null; then
     sudo flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
 fi
 log "End Communication"
 
+display "Start Functional Program"
 
-display "Start functionnal programe"
-
-# tlp
+# TLP
 sudo dnf install tlp tlp-rdw
 sudo systemctl enable tlp.service
-
 log "End tlp"
 
-# rofi
+# Rofi
 sudo dnf -y install rofi
-
 log "End rofi"
 
-# criterion
-sudo ./$SCRIPT_DIR/criterion/install_criterion.sh
-
-log "End criterion"
-
+# DNF plugins
 sudo dnf install -y dnf-plugins-core
 
-
-#set keyboard shortcut
+# Set keyboard shortcut
 sudo xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Super>r" -n -t string -s "kitty"
-sudo xfconf-query -c xfce4-keyboard-shortcuts -p "commmands/custom/<Super>f" -n -t string -s "rofi -show drun"
+sudo xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Super>f" -n -t string -s "rofi -show drun"
 
+# Install VSCode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf check-update
+sudo dnf check-update
 sudo dnf -y install code
+
